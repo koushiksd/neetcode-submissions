c5628a50f078e4c6128468737120b07761aa58d8
Koushik S D <48405582+koushiksd@users.noreply.github.com>
2026-09-16 13:13:12 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let n = nums.length
+        let output = new Array(n)
+        let prefix = 1
+        for(let i=0;i<n;i++){
+            output[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        let postfix = 1;
+        for (let i = n - 1; i >= 0; i--) {
+            output[i] *= postfix;
+            postfix *= nums[i];
+        }
+        
+        return output;
+    }
+}
